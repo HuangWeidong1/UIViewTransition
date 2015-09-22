d9caeb385e6e5ad14b03c43d0eb1d5d398044092
gyf5267 <gyf5267218@163.com>
2015-09-22 21:04:39 +0800

    Initial Commit

---
+++ UIViewTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewTransition
+//
+//  Created by nate on 15/9/22.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
